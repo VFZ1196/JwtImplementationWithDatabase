57b2be330d2dd01160d346beb5e2c03a8ccd217c
arjun <arjunyadav102030@gmail.com>
2021-02-17 16:47:38 +0530

    initial commit

---
+++ Test-Jwt-Implementation/src/main/java/com/jwtTest/TestJwtImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.jwtTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJwtImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJwtImplementationApplication.class, args);
+	}
+
+}
---
+++ Test-Jwt-Implementation/src/test/java/com/jwtTest/TestJwtImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwtTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJwtImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
